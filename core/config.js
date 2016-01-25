e6058a539e8942c88fd9934bfb944ec74c951563
Angelo Calvo Alfaro <angelo.calvoa@gmail.com>
2016-01-25 17:39:50 -0300

    ATUALIZACION

--- core/config.js
+++ core/config.js
@@ -1,33 +1,73 @@
 var service = require('./services.js');
+var stack = require('./stack.js');
+var _GLOBAL_CONFIG =  {};
+
 var methods = {
-	iptables: function(STACK, INDEX, CALLBACK){
-		if(typeof INDEX == "function") CALLBACK = INDEX, INDEX = 0;
-		if(typeof INDEX == "undefined") INDEX = 0;
-		if(typeof STACK[INDEX] != "undefined"){
-			service.exec("iptables "+STACK[INDEX]);
-			methods.iptables(STACK, (INDEX+1), CALLBACK);
-		}
-		else
-		{
-			if(typeof CALLBACK != "undefined") CALLBACK();
-			return;
-		}
-	}
-};
-module.exports = {
-	load: function(callback){
+	iptables: function(args){
 		console.log("CARGANDO CONFIGURACIONES");
-		service.fetch('./config/config.json', function(err,datos) {
+		service.fetch('./config/iptables.json', function(err,datos) {
 			if(err) {
-				console.log("ERROR LA HOJA DE CONFIGURACIONES NO EXISTE");
+				console.log("ERROR LA HOJA DE CONFIGURACIONES IPTABLES, NO EXISTE.");
 				service.close();
 			}
 			var CONFIG = JSON.parse(datos);
 			// INTEGRAMOS LOS PARAMETROS IPTABLES
-			console.log("CONFIGURANDO IPTABLES...");
-			methods.iptables(CONFIG.IPTABLES, function(){
-			});
-			if(typeof callback != "undefined") callback();
+			console.log("CARGANDO CONFIGURACIÓN IPTABLES...");
+			/*methods.iptables(CONFIG.IPTABLES, function(){
+			});*/
+			_GLOBAL_CONFIG['IPTABLES'] = CONFIG;
+			var callback = stack.unqueued();
+			if(typeof callback != "undefined") callback.action(callback.args);
 	    });
+	},
+	httpServer: function(callback){
+		service.fetch('./config/webserver.json', function(err,datos) {
+			if(err) {
+				console.log("ERROR LA HOJA DE CONFIGURACIONES WEBSERVER, NO EXISTE.");
+				service.close();
+			}
+			var CONFIG = JSON.parse(datos);
+			// INTEGRAMOS LOS PARAMETROS IPTABLES
+			console.log("CARGANDO CONFIGURACIÓN WEBSERVER...");
+			/*methods.iptables(CONFIG.IPTABLES, function(){
+			});*/
+			_GLOBAL_CONFIG['WEBSERVER'] = CONFIG;
+			var callback = stack.unqueued();
+			if(typeof callback != "undefined") callback.action(callback.args);
+	    });
+	},
+	CRMServer: function(callback){
+		service.fetch('./config/webserver.json', function(err,datos) {
+			if(err) {
+				console.log("ERROR LA HOJA DE CONFIGURACIONES CRMSERVER, NO EXISTE.");
+				service.close();
+			}
+			var CONFIG = JSON.parse(datos);
+			// INTEGRAMOS LOS PARAMETROS IPTABLES
+			console.log("CARGANDO CONFIGURACIÓN WIFREE-CRM...");
+			/*methods.iptables(CONFIG.IPTABLES, function(){
+			});*/
+			_GLOBAL_CONFIG['CRMSERVER'] = CONFIG;
+			var callback = stack.unqueued();
+			if(typeof callback != "undefined") callback.action(callback.args);
+	    });
+	},
+	continue: function(args){
+		if(typeof args != "undefined" && typeof args.callback != "undefined") args.callback();
+	},
+	load: function(cb){
+		stack.queued(methods.iptables);
+		stack.queued(methods.httpServer);
+		stack.queued(methods.CRMServer);
+		stack.queued(methods.continue, {
+			callback: cb
+		});
+		stack.start();
+	},
+	getConfig: function(){
+		return _GLOBAL_CONFIG;
 	}
 };
+//cargamos los modelos a exportar
+module.exports.load = methods.load;
+module.exports.getConfig = methods.getConfig;
--- core/webserver.js
+++ core/webserver.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var config  = require('./config.js');
 module.exports = {
 	start: function(){
+		var WS_CONFIG = config.getConfig();
 		var app = express();
 		app.get('/', function (req, res) {
 		  res.send('Hello World!');
 		});
 		console.log("SERVIDOR WEB INICIADO")
-		var server = app.listen(7000, function () {
+		var server = app.listen(WS_CONFIG.WEBSERVER.PORT, function () {
 		});
 	}
 }
