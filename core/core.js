5ea29dd1e6fcb2f206e3a3f53c116cc789f0e017
Angelo Calvo Alfaro <angelo.calvoa@gmail.com>
2016-01-27 13:06:10 -0300

    SE CORRIGEN NORMAS DE IPTABLES

--- core/core.js
+++ core/core.js
@@ -1,11 +1,15 @@
 var config  = require('./config.js');
 var iptables  = require('./iptables.js');
+var stack = require('./stack.js');
+var webserver = require('./webserver.js');
 var methods = {
 	start: function(){
 		var cf = config.getConfig();
 		config.setEnv(cf.GLOBAL.ENVIRONMENT);
 		console.log("\nIniciando WIFREE en modo "+cf.GLOBAL.ENVIRONMENT);
-		iptables.start();
+		stack.queued(iptables.start);
+		stack.queued(webserver.start);
+		start.start();
 	}
 }
 // INDICAMOS LOS MODULOS A EXPORTAR
