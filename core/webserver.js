02bf45f5c626f02eefc27453e59f3d9297d788fe
Angelo Calvo Alfaro <angelo.calvoa@gmail.com>
2016-02-16 13:26:22 -0300

    PUSH

--- core/webserver.js
+++ core/webserver.js
@@ -17,7 +17,7 @@ module.exports = {
 		    });
 		});
 		app.get('/*', function(req, res) {
-			console.log(req.path);
+			console.log(req.originalUrl);
 			if(req.url.indexOf("/wifree/") == 0){
 				res.sendFile(path.join(__dirname+"/../webapp"+ '/'+req.url.replace("/wifree/", "")), function(err){
 			    	if (err) {
