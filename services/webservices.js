68610147dfa03917880dba44aa6b97f483330af7
Angelo Calvo Alfaro <angelo.calvoa@gmail.com>
2016-02-17 16:48:48 -0300

    SE AGREGAN LOS SERVICIOS DE ENRUTADO

--- services/webservices.js
+++ services/webservices.js
@@ -3,6 +3,6 @@ var config  = require('../core/config.js');
 var iptables = require('../core/iptables.js');
 module.exports = {
 	allow: function(datos){
-		console.log("estamos en allow");
+		console.log(datos);
 	}
 };
