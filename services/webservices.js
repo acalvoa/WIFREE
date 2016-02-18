e66b34c421fb888fffe6fe599916574f8859d38c
Angelo Calvo Alfaro <angelo.calvoa@gmail.com>
2016-02-18 10:48:57 -0300

    SE CORRIGE LA REGLA IPTABLES

--- services/webservices.js
+++ services/webservices.js
@@ -2,9 +2,13 @@ var express = require('express');
 var config  = require('../core/config.js');
 var iptables = require('../core/iptables.js');
 module.exports = {
-	allow: function(datos, callback){
-		iptables.allow(datos.mac, function(){
+	allow: function(req,res,callback){
+		iptables.allow(req.query.mac, function(){
 			if(typeof callback != "undefined") callback();
 		});
+	},
+	allowme: function(req,res,callback){
+		console.log(req.connection.remoteAddress);
+		iptables.getMACfromIP(req.connection.remoteAddress);
 	}
 };
