6a152ff3e6af1303f02077ca63d6c9a9a432be3d
Simon <simon.ig13@gmail.com>
2016-04-07 11:13:30 -0300

    add webapp

---
+++ webapp/src/app/facelog/facelog.controller.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('webapp')
+    .controller('FaceLogController', FaceLogController);
+
+  /** @ngInject */
+  function FaceLogController() {
+    var vm = this;
+
+
+})();
